dbd96c6efd6f5cf6121dfc0a25dfea0569693d12
ShahistaAfreen <na21b050@smail.iitm.ac.in>
2024-07-04 19:53:47 +0530

    chages

--- src/pages/Home/What.jsx
+++ src/pages/Home/What.jsx
@@ -36,24 +36,19 @@ function What() {
             <div className="swiper card_slider">
                 <div className="swiper-wrapper">
                     <div className="swiper-slide card">
-                        <span className="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-                        <span className="student-name">Jane Smith</span>
-                        <span className="student-college">XYZ College</span>
+                        <span className="review">Believe me when I say that Tensors is the best mock exam for JEE aspirants. It gives us a clear cut explanation about how the original exam would be. Through the Tensors exam I was able to judge what would be my stand amongst others in the state in jee</span>
+                        <span className="student-name">Lean Jose</span>
+                        <span className="student-college">IIT Madras</span>
                     </div>
                     <div className="swiper-slide card">
-                        <span className="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-                        <span className="student-name">Alice Johnson</span>
-                        <span className="student-college">PQR Institute</span>
+                        <span className="review">By Tensors, I was able to identify the chinks in my JEE preparation, literally. Tensors is so balanced inspite of being very detailed, that I got the true feel of writing the JEE exams. It developed my confidence and kept the tempo going in the last stages of preparation</span>
+                        <span className="student-name">Shafil Maheen</span>
+                        <span className="student-college">IISC Bangalore</span>
                     </div>
                     <div className="swiper-slide card">
-                        <span className="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
+                        <span className="review">When you take a mock test you expect it to live up to the standards of the original. Tensors, probably because it was made by IITians with an intent to kill, broke that standard. Solving such a paper boosted my confidence due to the simple fact that</span>
                         <span className="student-name">John Doe</span>
                         <span className="student-college">ABC University</span>
-                    </div>
-                    <div className="swiper-slide card">
-                        <span className="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-                        <span className="student-name">Mary Brown</span>
-                        <span className="student-college">LMN College</span>
                     </div>
                 </div>
                 <div className="swiper-button-next"></div>

0ef3270856a78c84861899f3b8f951ea6e70075a
ShahistaAfreen <na21b050@smail.iitm.ac.in>
2024-07-04 16:32:02 +0530

    committed chnges in what css n js

--- src/pages/Home/What.jsx
+++ src/pages/Home/What.jsx
@@ -1,71 +1,66 @@
 import "./what.css"
+import Swiper from 'swiper';
+import 'swiper/css';
+
 function What() {
-    //js code
-    document.addEventListener('DOMContentLoaded', () => {
-        const swiper = new Swiper('.swiper-container', {
-          slidesPerView: 1,
-          spaceBetween: 16,
-          loop: true,
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-          breakpoints: {
-            600: {
-              slidesPerView: 1,
-            },
-            900: {
-              slidesPerView: 2,
-            },
-            1100: {
-              slidesPerView: 3,
-            },
-          },
-          autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
-        });
-      });
+    document.addEventListener('DOMContentLoaded', () => {});
       
     return <>
-    <div class="wrapper">
-      <div class="swiper-container">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide card">
-            <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-            <span class="student-name">John Doe</span>
-            <span class="student-college">ABC University</span>
-          </div>
-          <div class="swiper-slide card">
-            <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-            <span class="student-name">Jane Smith</span>
-            <span class="student-college">XYZ College</span>
-          </div>
-          <div class="swiper-slide card">
-            <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-            <span class="student-name">Alice Johnson</span>
-            <span class="student-college">PQR Institute</span>
-          </div>
-          <div class="swiper-slide card">
-            <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-            <span class="student-name">Alice Johnson</span>
-            <span class="student-college">PQR Institute</span>
-          </div>
-          <div class="swiper-slide card">
-            <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
-            <span class="student-name">Alice Johnson</span>
-            <span class="student-college">PQR Institute</span>
-          </div>
+     <div class="wrapper">
+        <div class="swiper card_slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide card">
+                    <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
+                    <span class="student-name">Jane Smith</span>
+                    <span class="student-college">XYZ College</span>
+                </div>
+                <div class="swiper-slide card">
+                    <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
+                    <span class="student-name">Alice Johnson</span>
+                    <span class="student-college">PQR Institute</span>
+                </div>
+                <div class="swiper-slide card">
+                    <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
+                    <span class="student-name">John Doe</span>
+                    <span class="student-college">ABC University</span>
+                </div>
+                <div class="swiper-slide card">
+                    <span class="review">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga veritatis exercitationem excepturi illo repudiandae, similique id et voluptatem.</span>
+                    <span class="student-name">Mary Brown</span>
+                    <span class="student-college">LMN College</span>
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-      </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script type="text/javascript">
+      var swiper = new Swiper(".card_slider", {
+        slidesPerView: 3,
+        spaceBetween: 20,  
+        pagination: {
+             el: ".swiper-pagination",
+             clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          900: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          600: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          }
+        }
+      });
+    </script>
     
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    
-    {/* <script src="script.js" defer></script> */}
     </>;
 }
 
--- src/pages/Home/Why.jsx
+++ src/pages/Home/Why.jsx
@@ -9,6 +9,21 @@ import "./why.css"
 function Why() {
     //js code 
     return <>
+    <div class="container">
+        <h1>Why Participate?</h1>
+        <div class="ellipse-container">
+            <div class="ellipse" id="left-ellipse">
+                <p>Holistic Learning<br>OurOlympiad is designed to test students' conceptual understanding, analytical skills, and problem-solving abilities.</p>
+            </div>
+            <img src="Young_man_thinking_about_the_question-removebg-preview.png" alt="Thinking Boy" class="center-image">
+            <div class="ellipse" id="right-ellipse">
+                <p>Future Ready<br>Online examinations prepare students for a digital world, mirroring the format of many competitive exams.</p>
+            </div>
+            <div class="ellipse" id="bottom-ellipse">
+                <p>Inclusivity<br>We welcome students from all boards including CBSE, ICSE, State Boards, IB, IGCSE, and ISC.</p>
+            </div>
+        </div>
+    </div>
     </>;
 }
 

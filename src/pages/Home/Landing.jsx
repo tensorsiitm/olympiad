a88fb5921f89bc58501b818c3bc9a9d2b01101c9
Narmi002 <shaiknarmi002@gmail.com>
2024-07-09 16:03:33 +0530

    responsive ness

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -1,16 +1,14 @@
-import ten_2 from '../../Images/ten_2.png'
-import ten_3 from '../../Images/ten_3.png'
+import cash_image from '../../Images/image 2.png'
 import trust from '../../Images/Trust.png'
 import school from '../../Images/ten_school.png'
 import place from '../../Images/Place Marker.png'
 import pen from '../../Images/Ball Point Pen.png'
 import users from '../../Images/Users.png'
-import ellipse from '../../Images/ellipse.png'
-import ellipse_1 from '../../Images/Ellipse 1.png'
-import ellipse_2 from '../../Images/ellipse 2.png'
 import '../../components/Navbar/Navbar.css'
 import similing from '../../Images/similing_woman.png'
 import flat_university from '../../Images/flat-university-background 1.png'
+import image_3 from '../../Images/image 3.png'
+import image_4 from '../../Images/image 4.png'
 
 function Landing() {
     return (
@@ -40,7 +38,7 @@ function Landing() {
 
             </section>
             <section className='third_section mb-4 '>
-                <div className='  py-[23px] bg-gradient-to-b from-white from-40% to-[rgba(0,158,134,0.22)] to-55%  '>
+                <div className=' info  py-[23px] bg-gradient-to-b from-white from-40% to-[rgba(0,158,134,0.22)] to-55%  '>
                     <div className='text-[rgb(37,161,142)]  font-bold'>ALL INDIA SCIENCE,MATHS AND APTITUDE OLYMPAIDS</div>
                     <div className=' text-[rgb(37,161,142)]'>FOR CLASS 6 TO 10</div>
                 </div>
@@ -52,8 +50,8 @@ function Landing() {
 
                             </div>
                             <ul className='flex flex-col  text-white  items-center'>
-                                <li className='text-[23px] font-bold mr-[58px]' >16+</li>
-                                <li className='text-[17px] mt-[-10px]'>Years of Trust</li>
+                                <li className='item-1 text-[23px] font-bold mr-[58px]' >15+</li>
+                                <li className='item-2 text-[17px] mt-[-10px]'>Years of Trust</li>
                             </ul>
                         </div>
                     </div>
@@ -64,8 +62,8 @@ function Landing() {
 
                             </div>
                             <ul className='flex flex-col  text-white  items-center'>
-                                <li className='text-[23px] font-bold '>5000+</li>
-                                <li className='text-[17px] mt-[-10px] ml-[-6px]'>schools</li>
+                                <li className='item-1 text-[23px] font-bold '>5000+</li>
+                                <li className='item-2 text-[17px] mt-[-10px] ml-[-6px]'>schools</li>
                             </ul>
                         </div>
                     </div>
@@ -76,8 +74,8 @@ function Landing() {
 
                             </div>
                             <ul className='flex flex-col  text-white  items-center'>
-                                <li className='text-[23px] font-bold'>16+</li>
-                                <li className='text-[17px] mt-[-10px]'>state</li>
+                                <li className='item-1 text-[23px] font-bold'>14+</li>
+                                <li className='item-2 text-[17px] mt-[-10px]'>state</li>
                             </ul>
                         </div>
                     </div>
@@ -88,8 +86,8 @@ function Landing() {
 
                             </div>
                             <ul className='flex flex-col  text-white  items-center'>
-                                <li className='text-[23px] font-bold'>500+</li>
-                                <li className='text-[17px]  mt-[-10px] '>Assessments</li>
+                                <li className='item-1 text-[23px] font-bold'>500+</li>
+                                <li className='item-2 text-[17px]  mt-[-10px] '>Assessments</li>
                             </ul>
                         </div>
                     </div>
@@ -100,8 +98,8 @@ function Landing() {
 
                             </div>
                             <ul className='flex flex-col  text-white  items-center'>
-                                <li className='text-[23px] font-bold'>10,000+</li>
-                                <li className='text-[17px] ml-[-36px] mt-[-10px]'>users</li>
+                                <li className='item-1 text-[23px] font-bold'>20,000+</li>
+                                <li className='item-2 text-[17px] ml-[-36px] mt-[-10px]'>users</li>
                             </ul>
                         </div>
                     </div>
@@ -115,29 +113,29 @@ function Landing() {
                 <div className='box_ flex py-2'>
                 <div className='box-1 w-1/3  m-4 border-2 rounded-lg '>
                 <div className='flex  flex-col items-center'>
-                            <img src={ellipse_2} alt="" className='w-[35%] ' />
-                            <h6 className='font-bold '>National Topper</h6>
+                            <img src={cash_image} alt="" className='w-[35%] ' />
+                            <h6 className='font-bold '>Cash Prizes</h6>
                             <div>
-                                <p className='text-xs px-3'>  A guided visit to the prestigious Indian </p>
-                                    <p className='text-xs '>Institute of Technology Madras, Gold Medal</p>
-                                    <p className='text-xs mb-1'> and National Topper Award Certificate.</p>
+                                <p className='text-xs px-3 font-bold'>1st:15000 2nd:10000 3rd:5000 </p>
+                                    <p className='text-xs '>In each grade</p>
                             </div>
                         </div>
                     </div>
-                    <div className='box-2  w-1/3  m-4 border-2 rounded-lg '>
+                    <div className='box-2  w-1/3  m-4 border-2 rounded-lg py-2 '>
                         <div className='flex  flex-col items-center'>
-                            <img src={ellipse} alt="" className='w-[35%] ' />
-                            <h6 className='font-bold '>State Topper</h6>
+                            <img src={image_3} alt="" className='w-[25%] ' />
+                            <h6 className='font-bold '>Campus Visit</h6>
                             <div>
-                                <p className='text-xs px-3 '>State Topper Award certificate </p>
-                                <p className='text-xs '>and Gold Medal</p>
+                                <p className='text-xs px-3 '>  A fully sponsored, guided visit to the </p>
+                                    <p className='text-xs px-3'>prestigious <b>Indian Institute of Technology </b></p>
+                                   <p className='text-xs px-3'> <b>Madras</b> for top 3 from each grade.</p>
                             </div>
                         </div>
                     </div>
-                    <div className='box-3  w-1/3  m-4 border-2 rounded-lg '>
+                    <div className='box-3  w-1/3  m-4 border-2 rounded-lg  py-4'>
                         <div className='flex  flex-col items-center'>
-                            <img src={ellipse_1} alt="" className='w-[35%] ' />
-                            <h6 className='font-bold  '>High Scorers</h6>
+                            <img src={image_4} alt="" className='w-[25%] ' />
+                            <h6 className='font-bold  '>Certificates</h6>
                             <div>
                                 <p className='text-xs px-3'>Students making it to top 10% gets best </p>
                                 <p className='text-xs '>performer Certificate</p>
